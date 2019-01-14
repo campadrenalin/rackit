15d4c55b9e23ac3c93d07391dc40fca221768d7b
Philip Horger <campadrenalin@gmail.com>
2019-01-14 02:30:26 -0800

    Actually generate sound

--- audio_lua.c
+++ audio_lua.c
@@ -37,29 +37,59 @@ static const struct luaL_Reg BufferMetatable[] = {
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_audio.h>
-const int AMPLITUDE = 15000;
+#include "math.h"
+const double PI = 3.141592653589793238463;
+const double TAU = 2*PI;
+const int AMPLITUDE = 13000;
 const int SAMPLE_RATE = 44100;
+const double sr = SAMPLE_RATE;
+const SDL_AudioFormat FORMAT_WANTED = AUDIO_S16SYS;
+typedef Sint16 FORMAT;
+Buffer master;
 
-int sdl_err(lua_State *L, const char* fmt) {
-    return luaL_error(L, fmt, SDL_GetError());
+typedef struct {
+    double phase;
+    double freq;
+    Buffer *out;
+} OscSine;
+
+void OscSine_process(OscSine *osc, int length) {
+    Buffer *out = osc->out;
+    double p = 0;
+    double f = osc->freq*TAU;
+    for(int i=0; i<length; i++) {
+        double t = (double)i/sr;
+        p = t*f;
+        (*out)[i] = sin(osc->phase + p);
+    }
+    osc->phase += p;
+    // SDL_Log("Phase: %f", osc->phase);
 }
+
+OscSine wave = {0, 440, &master};
 
 #include "time.h"
 void audio_callback(void *user_data, Uint8 *raw_buffer, int bytes) {
     struct timespec start, end;
+
+    FORMAT *buffer = (FORMAT*)raw_buffer;
+    int length = bytes/2;
+
     clock_gettime(CLOCK_MONOTONIC, &start);
-
-    double *buffer = (double*)raw_buffer;
-    int length = bytes / sizeof(double);
-
-    for (int i=0; i<length; i++) {
-        buffer[i] = 0;
-    }
-
+    SDL_zero(master);
+    OscSine_process(&wave, length);
+    for (int i=0; i<length; i++)
+        buffer[i] = master[i]*AMPLITUDE;
     clock_gettime(CLOCK_MONOTONIC, &end);
+
     time_t seconds = end.tv_sec - start.tv_sec;
     long nanos = (seconds * 1e9)  + end.tv_nsec - start.tv_nsec;
-    SDL_Log("Produced %d samples in %ld nanoseconds", length, nanos);
+    // SDL_Log("Produced %d samples (%d bytes) in %f seconds, first value %d",
+            // length, bytes, (double)nanos/1e9, buffer[0]);
+}
+
+int sdl_err(lua_State *L, const char* fmt) {
+    return luaL_error(L, fmt, SDL_GetError());
 }
 
 static int sdl_init (lua_State *L) {
@@ -69,14 +99,18 @@ static int sdl_init (lua_State *L) {
     SDL_AudioSpec want, have;
     SDL_zero(want);
     want.freq = SAMPLE_RATE;
-    want.format = AUDIO_F32SYS;
+    want.format = FORMAT_WANTED;
     want.channels = 1;
     want.samples = BUFFER_SIZE;
     want.callback = audio_callback;
     if (SDL_OpenAudio(&want, &have) != 0)
         return sdl_err(L, "Failed to open audio: %s");
+    // SDL_Log("Got: freq %d, format %d, channels %d, silence %d, samples %d, size %d",
+            // have.freq, have.format, have.channels, have.silence, have.samples, have.size);
     if (want.format != have.format)
         return sdl_err(L, "Failed to get the desired AudioSpec");
+    if (want.freq != have.freq)
+        return sdl_err(L, "Failed to get the desired frequency");
     return 0;
 }
 
--- audio_test.lua
+++ audio_test.lua
@@ -4,7 +4,12 @@ buf = lib.Buffer()
 assert(type(buf) == 'userdata', "Buffer is a userdata")
 assert(type(getmetatable(buf).peek) == 'function', "Metatable is not nil")
 assert(buf:peek() == 0, "First element is zero")
-print("Tests pass - OK")
 
 buf = lib.Buffer(32)
 assert(buf:peek() == 32, "First element is 32")
+
+print("Playing sound")
+lib.sdl_init()
+lib.sdl_play(1000)
+lib.sdl_finish()
+print("Tests pass - OK")
